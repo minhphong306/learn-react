3e7a1bfdb848204c65226cb75d0c1ed0e25921fd
PhongDo <phongdo@beeketing.net>
2020-08-05 21:59:50 +0700

    Begin

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Item from './components/item'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.items = ['job1', 'job2', 'job3'];
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div>A simple TODO app</div>
+                {
+                    this.items.map((item, index) => <Item key={index} title={item}/>)
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
