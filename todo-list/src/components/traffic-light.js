4e04895e3ff6d5253ef23dbb919bc068d119de0f
PhongDo <phongdo@beeketing.net>
2020-08-05 22:17:25 +0700

    Test markdown

---
+++ todo-list/src/components/traffic-light.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TrafficLight extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TrafficLight;
